286fcdd86b7c5cfea412d9e80e1bac0f8a7f6dae
jtshivers <jacobshivers@protonmail.com>
2022-03-28 20:51:49 -0500

    Defined Student and Campus models and associations

---
+++ server/db/campus.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const db = require("./database");
+
+const Campus = db.define("campus", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://tinyurl.com/stockcampus",
+    validate: {
+      isUrl: true,
+      notEmpty: true,
+    },
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      "The lobsters have risen! We've been too occupied with battle to finish typing a description.",
+  },
+});
+
+module.exports = Campus;
